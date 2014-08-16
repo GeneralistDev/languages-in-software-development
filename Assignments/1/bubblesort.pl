303c5550130fd6352048677aa3888443846b6f53
Daniel Parker <dparker.tech@gmail.com>
2014-08-16 16:31:45 +1000

    NEW: Assignment 1, part 2, no syntax hightlighting

---
+++ Assignments/1/bubblesort.pl
@@ -0,0 +1,29 @@
+
+% Bubble list from left to right
+% 
+% LOGIC: 
+%	Check the head against the head of the tail
+%	swapping the heads if the first head is bigger than the second head.
+%	recursively bubble the remaining tail with the correct swapped head.
+%
+bubble([],[]).
+bubble([X],[X]).
+bubble([X,Y|T], [Y|Z]) :- X > Y, bubble([X|T], Z).
+bubble([X,Y|T], [X|Z]) :- X =< Y, bubble([Y|T], Z).
+
+% Remove the head
+remove([H|T],H,T).
+
+% Bubble sort the whole list
+%
+% LOGIC:
+%	Bubble the list, reverse it and remove the head.
+%	Bubble the tail of the resulting list. Repeat until all parts are sorted.
+bubble_sort([],[]). 
+bubble_sort([X], [X]).
+bubble_sort(X,[H|L]) :- bubble(X, Y),
+						reverse(Y, Z), 
+						remove(Z,H,T), 
+						reverse(T,C), 
+						bubble_sort(C, L). 
+
